6f5c9c30cebe1039ebfab5a327c5fea91ee9262d
Yulied Casas <yacasas05@misena.edu.co>
2020-10-05 01:09:41 -0500

    Captura los datos por pantalla

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace Laboratorio03GitHub
             Console.WriteLine("Hello World!");
             /*solicito el nombre del usuario*/
             Console.WriteLine("\nWhat is your name");
+
+            /*Capturo los datos (Nombre y fecha ) en unas variables*/
+
+            var name= Console.ReadLine();
+            var date=DateTime.Now;
+
+
+
+
         }
     }
 }
